131b2bcf57be39247af415aef2f5c30846e0e328
ian952 <ianhu952@hotmail.com>
2017-04-02 00:34:24 -0700

    remove fade in effect on about

--- scripts/about.js
+++
@@ -1,6 +0,0 @@
-const PROFILE_HEADER_FADE_TIME = 700;
-
-$(document).ready (function() {
-	$('#profile-header').css('visibility','visible').hide().fadeIn(PROFILE_HEADER_FADE_TIME, function () {
-	});
-});
